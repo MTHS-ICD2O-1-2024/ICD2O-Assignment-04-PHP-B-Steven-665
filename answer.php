e10f818ba26754081a1d704178855b21e5e23d91
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-06-10 16:28:25 +0000

    A4 php

---
+++ answer.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Sushi order result" />
+  <meta name="author" content="yi.zhou" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <title>Sushi Order Result</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Sushi Order Result</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="page-content-answer">
+        <br /><br />
+
+        <?php
+        // Prices
+        $regularPrice = 5.99;
+        $bigPrice = 7.99;
+        $salmonPrice = 6.99;
+        $tunaPrice = 7.99;
+        $arcticClamPrice = 6.99;
+        $sweetShrimpPrice = 5.99;
+        $taxRate = 0.13;
+
+        // Get input
+        $size = $_GET["size"];
+        $material = $_GET["material"];
+
+        // Size price
+        if ($size == "regular") {
+          $sizePrice = $regularPrice;
+        } else {
+          $sizePrice = $bigPrice;
+        }
+
+        // Material price
+        switch ($material) {
+          case "salmon":
+            $materialPrice = $salmonPrice;
+            break;
+          case "tuna":
+            $materialPrice = $tunaPrice;
+            break;
+          case "arctic-clam":
+            $materialPrice = $arcticClamPrice;
+            break;
+          case "sweet-shrimp":
+            $materialPrice = $sweetShrimpPrice;
+            break;
+          default:
+            $materialPrice = 0;
+        }
+
+        // Total
+        $totalPrice = $sizePrice + $materialPrice;
+        $finalPrice = $totalPrice * (1 + $taxRate);
+
+        echo "<p>The Size Price is: <strong>$" . number_format($sizePrice, 2) . "</strong></p>";
+        echo "<p>The Food Material Price is: <strong>$" . number_format($materialPrice, 2) . "</strong></p>";
+        echo "<p>The Total Price is: <strong>$" . number_format($totalPrice, 2) . "</strong></p>";
+        echo "<p>After Tax is: <strong>$" . number_format($finalPrice, 2) . "</strong></p>";
+        ?>
+
+        <br /><br />
+        <a href="index.php" class="mdl-button mdl-js-button mdl-button--raised">Order Again</a>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,67 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Buy a sushi, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="yi.zhou" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Buy a sushi, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Buy a sushi, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/Sushi_platter.jpg" alt="sushi image" />
+      </div>
+      <br />
+      <div class="page-content-1">Please select the size you want</div>
+      <br /><br />
+      <form action="answer.php" method="get">
+        <!-- Radio buttons for sushi size -->
+        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="regular-size">
+          <input type="radio" id="regular-size" class="mdl-radio_button" name="size" value="regular" checked>
+          <span class="mdl-radio_label">Regular</span>
+        </label>
+        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="big-size">
+          <input type="radio" id="big-size" class="mdl-radio_button" name="size" value="big">
+          <span class="mdl-radio_label">Big</span>
+        </label>
+        <br /><br />
+
+        <!-- Dropdown for material -->
+        <div id="option" class="dropdown-container">
+          <label class="select-day" for="food-material">Please select your food material</label><br><br>
+          <select id="food-material" name="material">
+            <option value="salmon">Salmon</option>
+            <option value="tuna">Tuna</option>
+            <option value="arctic-clam">Arctic clam</option>
+            <option value="sweet-shrimp">Sweet shrimp</option>
+          </select>
+        </div>
+        <br /><br />
+
+        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+          ORDER
+        </button>
+      </form>
+    </main>
+  </div>
+</body>
+
 </html>
